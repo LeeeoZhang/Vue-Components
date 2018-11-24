f18e0a3bc31b5ddcf3325900a775ab1c4972c34d
Leo Cheung <leozhang621@gmail.com>
2018-11-24 17:53:39 +0800

    feat:添加Divider组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <Divider title="你好吗"/>
+        <router-view/>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Divider from './components/Divider/Divider'
+
+  export default {
+    components: {
+      Divider,
+    },
+    name: 'App'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Divider/Divider.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="divider">
+        <span class="divider-title">{{title}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+      props:['title'],
+    }
+</script>
+
+<style>
+    .divider {
+        font-size: 16px;
+        font-weight: 500;
+        display: flex;
+    }
+    .divider > .divider-title {
+        padding: 0 10px;
+    }
+    .divider:before,
+    .divider:after {
+        content: ' ';
+        display: block;
+        padding-bottom: 23px;
+        border-top: 1px solid #e8e8e8;
+        transform: translateY(50%);
+    }
+    .divider:before {
+        width: 1em;
+    }
+    .divider:after {
+        flex: 1;
+    }
+</style>
